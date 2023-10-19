e89684a7aad750493302fbcb439cc5761685fa77
Saeedehhosinny <148279358+Saeedehhosinny@users.noreply.github.com>
2023-10-19 23:04:45 +0330

    Add files via upload

---
+++ tamrin1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the age of your mother:"))
+b = int(input("Enter your age:"))
+r = a-b
+print(r)
+if r>=15:
+    input("you are probably the first child and you have a young mother")
+if r>=25:
+    input("you are probably the second child")
+if r>=35:
+    input("you are probably the third child")
+print(r)
